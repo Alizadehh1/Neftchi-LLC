ee8b154919b04265015017232c59e87b6f2c2d91
alizadehh <alisal@code.edu.az>
2025-03-29 12:56:32 +0200

    Add frontend folder

---
+++ backend/NeftchiLLCSolution/NeftchiLLC.Application/Services/AzureBlobService.cs
@@ -0,0 +1,27 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace NeftchiLLC.Application.Services
+{
+	public class AzureBlobService
+	{
+		private readonly BlobContainerClient _container;
+
+		public AzureBlobService(IConfiguration config)
+		{
+			var connection = config["AzureBlobStorage:ConnectionString"];
+			var containerName = config["AzureBlobStorage:ContainerName"];
+			_container = new BlobContainerClient(connection, containerName);
+			_container.CreateIfNotExists();
+		}
+
+		public async Task<string> UploadAsync(IFormFile file)
+		{
+			var blob = _container.GetBlobClient(file.FileName);
+			await using var stream = file.OpenReadStream();
+			await blob.UploadAsync(stream, true);
+			return blob.Uri.ToString();
+		}
+	}
+}
