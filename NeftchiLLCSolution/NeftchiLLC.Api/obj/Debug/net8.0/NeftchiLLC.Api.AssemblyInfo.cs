09dfc0ce906f0722638d41efee61884ef48771d7
Huseyn95 <Huseyn.agayev919@gmail.com>
2025-04-02 17:40:03 +0400

    DocumentsEnd

--- NeftchiLLCSolution/NeftchiLLC.Api/obj/Debug/net8.0/NeftchiLLC.Api.AssemblyInfo.cs
+++ NeftchiLLCSolution/NeftchiLLC.Api/obj/Debug/net8.0/NeftchiLLC.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NeftchiLLC.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd00d39562531d541446cfb21d13a78f51612a23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+286cef245898381c72e37b265cc744447beece11")]
 [assembly: System.Reflection.AssemblyProductAttribute("NeftchiLLC.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NeftchiLLC.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
