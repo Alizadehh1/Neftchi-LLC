60b1fe3dd3bab8905a80de01024fc7534e584dd9
Aliizadehh <alisal@code.edu.az>
2025-05-10 17:54:50 +0300

    done

--- frontend/src/components/Equipments/index.tsx
+++ frontend/src/components/Equipments/index.tsx
@@ -1,6 +1,6 @@
 import { useRef } from 'react'
 import style from './index.module.scss';
-import { FaArrowRight, FaArrowLeft } from "react-icons/fa6";
+// import { FaArrowRight, FaArrowLeft } from "react-icons/fa6";
 import { useNavigate } from 'react-router-dom';
 import { EQUIPMENT_PATH } from '../../utils/routes';
 import { photos } from './utils';
@@ -11,15 +11,15 @@ const Equipments = () => {
 
     const imageListRef = useRef<HTMLDivElement | null>(null);
 
-    const scroll = (direction: "left" | "right") => {
-        if (!imageListRef.current) return;
+    // const scroll = (direction: "left" | "right") => {
+    //     if (!imageListRef.current) return;
 
-        const scrollAmount = imageListRef.current.clientWidth * 0.2;
-        imageListRef.current.scrollBy({
-            left: direction === "right" ? scrollAmount : -scrollAmount,
-            behavior: "smooth",
-        });
-    };
+    //     const scrollAmount = imageListRef.current.clientWidth * 0.2;
+    //     imageListRef.current.scrollBy({
+    //         left: direction === "right" ? scrollAmount : -scrollAmount,
+    //         behavior: "smooth",
+    //     });
+    // };
 
     return (
         <div className={style.equipment}>
