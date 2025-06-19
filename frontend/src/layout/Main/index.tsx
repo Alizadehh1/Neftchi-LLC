d5bbc23173569b83a46812b050b7d3b2ea141a9d
banch0u <fikret.okis@gmail.com>
2025-06-19 15:49:07 +0400

    wrapper added

--- frontend/src/layout/Main/index.tsx
+++ frontend/src/layout/Main/index.tsx
@@ -1,20 +1,19 @@
-import  { ReactNode } from 'react'
-import Header from '../Header'
-import Footer from '../Footer'
+import { ReactNode } from "react";
+import Header from "../Header";
+import Footer from "../Footer";
 
 interface IProps {
-    children: ReactNode
+  children: ReactNode;
 }
 
 const Main = ({ children }: IProps) => {
-    return (
-        <>
-            <Header />
-            {children}
-            <Footer />
-        </>
+  return (
+    <div style={{ maxWidth: 1440, margin: "auto", width: "75%" }}>
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
+};
 
-    )
-}
-
-export default Main
+export default Main;
