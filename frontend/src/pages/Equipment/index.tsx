b2ec59a1d7ab05b5264f0a5013dca3b015fbe76d
banch0u <fikret.okis@gmail.com>
2025-06-19 17:53:50 +0400

    image fixes in various pages

--- frontend/src/pages/Equipment/index.tsx
+++ frontend/src/pages/Equipment/index.tsx
@@ -1,15 +1,14 @@
-import style from './index.module.scss';
-import Main from '../../layout/Main';
-import { Input } from 'antd';
+import style from "./index.module.scss";
+import Main from "../../layout/Main";
+import { Input } from "antd";
 import { IoSearchSharp } from "react-icons/io5";
-import { IEquipment } from './types';
-import axios from 'axios';
-import { baseUrl } from '../../utils/baseUrl';
-import { useEffect, useRef, useState } from 'react';
-import Loading from '../../components/Loading/Loading';
-import { FaArrowLeft, FaArrowRight } from 'react-icons/fa6';
-import { photos } from '../../components/Equipments/utils';
-
+import { IEquipment } from "./types";
+import axios from "axios";
+import { baseUrl } from "../../utils/baseUrl";
+import { useEffect, useRef, useState } from "react";
+import Loading from "../../components/Loading/Loading";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa6";
+import { photos } from "../../components/Equipments/utils";
 
 const Equipment = () => {
   const imageListRef = useRef<HTMLDivElement | null>(null);
@@ -17,7 +16,6 @@ const Equipment = () => {
   const [loading, setLoading] = useState(false);
   const [equipments, setEquipments] = useState<IEquipment[]>([]);
   const [inputValue, setInputValue] = useState("");
-
 
   const scroll = (direction: "left" | "right") => {
     if (!imageListRef.current) return;
@@ -31,24 +29,23 @@ const Equipment = () => {
 
   const fetchData = async () => {
     setLoading(true);
-    await axios.get(baseUrl + `/equipments?searchTerm=${inputValue}`).then(res => {
-      setEquipments(res?.data?.data)
-    })
+    await axios
+      .get(baseUrl + `/equipments?searchTerm=${inputValue}`)
+      .then((res) => {
+        setEquipments(res?.data?.data);
+      });
     setLoading(false);
-  }
+  };
 
   useEffect(() => {
     fetchData();
-  }, [inputValue])
+  }, [inputValue]);
 
-
-  if (loading) return <Loading />
+  if (loading) return <Loading />;
 
   return (
     <Main>
-
       <div className={style.tableEquipment}>
-
         <h2>Texnika və avadanlıqlar</h2>
 
         <table className={style.table}>
@@ -60,32 +57,34 @@ const Equipment = () => {
               <th>Model</th>
               <th>Təyinatı</th>
               <th className={style.inputTd}>
-                <Input onChange={e => setInputValue(e?.target?.value)} prefix={<IoSearchSharp />} />
+                <Input
+                  onChange={(e) => setInputValue(e?.target?.value)}
+                  prefix={<IoSearchSharp />}
+                />
               </th>
             </tr>
           </thead>
-          <tbody >
+          <tbody>
             {equipments?.map((equipment: IEquipment) => (
-              <tr key={equipment.id} >
-                <td >{equipment.name}</td>
+              <tr key={equipment.id}>
+                <td>{equipment.name}</td>
                 <td>{equipment?.quantity}</td>
-                <td >{equipment?.model}</td>
-                <td >{equipment?.description}</td>
-                <td ></td>
+                <td>{equipment?.model}</td>
+                <td>{equipment?.description}</td>
+                <td></td>
               </tr>
             ))}
           </tbody>
         </table>
-
       </div>
-
 
       <div className={style.equipment}>
         <h2>Avadanlıqlar</h2>
 
         <div className={style.equipmentImages}>
-
-          <div onClick={() => scroll("left")} className={style.equipmentRightArrow}>
+          <div
+            onClick={() => scroll("left")}
+            className={style.equipmentRightArrow}>
             <FaArrowLeft />
           </div>
 
@@ -95,20 +94,17 @@ const Equipment = () => {
                 <img src={value?.photo} />
               </figure>
             ))}
-
           </div>
 
-          <div onClick={() => scroll("right")} className={style.equipmentLeftArrow}>
+          <div
+            onClick={() => scroll("right")}
+            className={style.equipmentLeftArrow}>
             <FaArrowRight />
           </div>
-
         </div>
-
       </div>
-
-
     </Main>
-  )
-}
+  );
+};
 
-export default Equipment
+export default Equipment;
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,31 +1,31 @@
-import style from './index.module.scss';
-import Main from '../../layout/Main';
-import Hero from "../../assets/Hero.png";
-import Partners from '../../components/Home/Partners';
-import Equipments from '../../components/Equipments';
-import AboutUses from '../../components/Home/AboutUses/AboutUses';
-import Services from '../../components/Home/Services/Services';
-import Projects from '../../components/Home/Projects/Projects';
+import style from "./index.module.scss";
+import Main from "../../layout/Main";
+import Hero from "../../assets/NewHero.jpg";
+import Partners from "../../components/Home/Partners";
+import Equipments from "../../components/Equipments";
+import AboutUses from "../../components/Home/AboutUses/AboutUses";
+import Services from "../../components/Home/Services/Services";
+import Projects from "../../components/Home/Projects/Projects";
 
 const Home = () => {
+  return (
+    <Main>
+      <div className={style.container}>
+        <div className={style.hero}>
+          <img src={Hero} alt={""} />
+          <h1>
+            Keyfiyyət bizim <br /> missiyamızdır!
+          </h1>
+        </div>
 
-    return (
-        <Main>
-            <div className={style.container}>
+        <AboutUses />
+        <Services />
+        <Equipments />
+        <Partners />
+        <Projects />
+      </div>
+    </Main>
+  );
+};
 
-                <div className={style.hero}>
-                    <img src={Hero} alt={""} />
-                </div>
-
-                <AboutUses />
-                <Services />
-                <Equipments />
-                <Partners />
-                <Projects />
-
-            </div>
-        </Main>
-    )
-}
-
-export default Home
+export default Home;
