b23082ce760a5948a96e8c4ee23e855ba3f675aa
banch0u <fikret.okis@gmail.com>
2025-07-27 23:45:13 +0400

    equipments added

--- frontend/src/components/Equipments/utils.tsx
+++ frontend/src/components/Equipments/utils.tsx
@@ -1,63 +1,112 @@
 import avtokran from "../../assets/avtokran.jpg";
-import Backloader from '../../assets/Backloader.jpg';
+import Backloader from "../../assets/Backloader.jpg";
 import buldozer from "../../assets/buldozer.jpg";
 import kamaz2 from "../../assets/kamaz2.jpg";
 import kamaz from "../../assets/kamaz-.jpg";
 import ekskovator from "../../assets/ekskovator.jpg";
-import ford from '../../assets/ford.jpg';
+import ford from "../../assets/ford.jpg";
 import kran from "../../assets/kran.jpg";
 import pickup from "../../assets/pick-up.jpg";
 import kamaz3 from "../../assets/kamaz3.jpg";
 import qreyder from "../../assets/qreyder.jpg";
-import trailercopy from "../../assets/trailercopy.jpg"
+import trailercopy from "../../assets/trailercopy.jpg";
+
+import equipment1 from "../../assets/equipments/avadanliq-1.jpg";
+import equipment2 from "../../assets/equipments/avadanliq-2.jpg";
+import equipment3 from "../../assets/equipments/avadanliq-3.jpg";
+import equipment4 from "../../assets/equipments/avadanliq-4.jpg";
+import equipment5 from "../../assets/equipments/avadanliq-5.jpg";
+import equipment6 from "../../assets/equipments/avadanliq-6.jpg";
+import equipment7 from "../../assets/equipments/avadanliq-7.jpg";
+import equipment8 from "../../assets/equipments/avadanliq-8.jpg";
+import equipment9 from "../../assets/equipments/avadanliq-9.jpg";
 
 export const photos = [
-    {
-        id: 1,
-        photo: avtokran
-    },
-    {
-        id: 2,
-        photo: Backloader
-    },
-    {
-        id: 3,
-        photo: buldozer
-    },
-    {
-        id: 4,
-        photo: kamaz2
-    },
-    {
-        id: 5,
-        photo: ekskovator
-    },
-    {
-        id: 6,
-        photo: ford
-    },
-    {
-        id: 7,
-        photo: kamaz
-    },
-    {
-        id: 8,
-        photo: kran
-    },
-    {
-        id: 9,
-        photo: pickup
-    },
-    {
-        id: 10,
-        photo: kamaz3
-    },
-    {
-        id: 11,
-        photo: qreyder
-    },
-    {
-        id: 12,
-        photo: trailercopy
-    }
-]
+  {
+    id: 1,
+    photo: avtokran,
+  },
+  {
+    id: 2,
+    photo: Backloader,
+  },
+  {
+    id: 3,
+    photo: buldozer,
+  },
+  {
+    id: 4,
+    photo: kamaz2,
+  },
+  {
+    id: 5,
+    photo: ekskovator,
+  },
+  {
+    id: 6,
+    photo: ford,
+  },
+  {
+    id: 7,
+    photo: kamaz,
+  },
+  {
+    id: 8,
+    photo: kran,
+  },
+  {
+    id: 9,
+    photo: pickup,
+  },
+  {
+    id: 10,
+    photo: kamaz3,
+  },
+  {
+    id: 11,
+    photo: qreyder,
+  },
+  {
+    id: 12,
+    photo: trailercopy,
+  },
+];
+
+export const photosEquipment = [
+  {
+    id: 1,
+    photo: equipment1,
+  },
+  {
+    id: 2,
+    photo: equipment2,
+  },
+  {
+    id: 3,
+    photo: equipment3,
+  },
+  {
+    id: 4,
+    photo: equipment4,
+  },
+  {
+    id: 5,
+    photo: equipment5,
+  },
+  {
+    id: 6,
+    photo: equipment6,
+  },
+  {
+    id: 7,
+    photo: equipment7,
+  },
+  {
+    id: 8,
+    photo: equipment8,
+  },
+  {
+    id: 9,
+    photo: equipment9,
+  },
+];
